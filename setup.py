210b16a027c4bd401ffca45c970ed97db0c6e22c
chenh <1433255094@qq.com>
2024-06-22 12:47:21 +0800

    fix: setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
-setup(name='chattts',
-      version='0.0.1',
-      author='2noise',
-      url='https://github.com/2noise/ChatTTS',
-      install_requires=['omegaconf>=2.3.0',
-                'torch>=2.1.0',
-                'tqdm',
-                'vector_quantize_pytorch',
-                'transformers>=4.41.1',
-                'vocos',
-                'IPython',
-                ],  # 定义依赖哪些模块
-      packages=find_packages(),  # 系统自动从当前目录开始找包
-      )
+
+setup(
+    name="chattts",
+    version="0.0.1",
+    author="2noise",
+    url="https://github.com/2noise/ChatTTS",
+    install_requires=[
+        "omegaconf>=2.3.0",
+        "torch>=2.1.0",
+        "tqdm",
+        "vector_quantize_pytorch",
+        "transformers>=4.41.1",
+        "vocos",
+        "IPython",
+    ],  # 定义依赖哪些模块
+    packages=["ChatTTS"],  # 系统自动从当前目录开始找包
+)
